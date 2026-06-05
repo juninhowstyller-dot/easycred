fb97026503bec4a68953abb725e502d2c23807e9
José Carlos <juninhowstyller@gmail.com>
2026-06-05 01:34:06 -0300

    Corrige React undefined

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 
-function Dashboard() {
-  return (
-    <div style={{ padding: 30 }}>
-      <h1>Easy Cred</h1>
-      <p>Login funcionando ✅</p>
-    </div>
-  );
-}
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Login />;
 }
 
 export default App;
